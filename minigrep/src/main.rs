1a5b0823fa966eac5172e56a500b42d5ab0104af
Taylor Bird <nsubordin81@gmail.com>
2022-07-10 17:54:56 -0400

    12.3 refactor minigrep, more idiomatic error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // std::env::args will panic if any argument contains invalid Unicode. we will always pass valid unicode chars for simplicity
@@ -8,7 +9,12 @@ fn main() {
     // can't rely on type inference for collect, need to explicitly say Vec<String>
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    // we can call unwrap_or_else here because new now returns a Result<T, E> instance
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        // manually exit with non-zero code. Less noisy than panic! macro
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -25,11 +31,11 @@ struct Config {
 }
 
 impl Config{
-    fn new(args: &[String]) -> Config {
-        // a non-idiomatic way to handle errors in Rust. this would be better to catch a mistake the dev made, like assert in other languages
-        // not so great for catching a mistake the user made.
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        // switching to returning an Err Result object
         if args.len() < 3 {
-            panic!("not enough arguments");
+            // error must always be a static lifetimed string
+            return Err("not enough arguments");
         }
         // not using references anymore, can't violate ownership rules providing slices to Config
         // most straightforward way to share these values with config is to clone them here. Copies of the data will be made.
@@ -39,7 +45,7 @@ impl Config{
         let filename = args[2].clone();
 
         // expression, no semicolon required, will be returned
-        Config { query, filename }
+        Ok(Config { query, filename }) 
     }
 
 }
